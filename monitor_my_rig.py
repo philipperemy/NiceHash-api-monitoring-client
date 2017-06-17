cff55a89a42688813c94c6506e818d87f49fbfb5
philipperemy <premy@reactive.co.jp>
2017-06-17 13:07:37 +0900

    WIP

--- monitor_my_rig.py
+++ monitor_my_rig.py
@@ -10,23 +10,14 @@ class AlertEmailSender:
         self.gmail_user = gmail_user
         self.gmail_password = gmail_password
         self.target_email = target_email
-        self.id_emails_sent = list()
 
     def send_email(self, email_subject, email_content):
-        email_id = email_subject + email_content
-        if len(self.id_emails_sent) > 0:  # we don't want to send duplicates every time we pull.
-            last_id_email_sent = self.id_emails_sent[-1]
-            if last_id_email_sent == email_id:
-                logger.debug('Email already sent for subject = {}, content = {}'.format(email_subject, email_content))
-                return
-
         send_email_notification(self.gmail_user,
                                 self.gmail_password,
                                 self.target_email,
                                 email_content,
                                 email_subject)
         logger.debug('Email sent for subject = {}, content = {}'.format(email_subject, email_content))
-        self.id_emails_sent.append(email_id)
 
 
 def run_monitoring_tool():
@@ -38,20 +29,28 @@ def run_monitoring_tool():
     nice_hash_client = NiceHashClient(addr)
     polling_interval_sec = 60  # 1 minute
     rig_names_to_monitor = c.RIG_HOSTNAMES
+    previous_rig_statuses = [True] * len(rig_names_to_monitor)  # all online by default.
+    rig_statuses = list(previous_rig_statuses)  # deep copy.
 
     while True:
         logger.debug('run_monitoring_tool() - RUNNING')
         rig_names, speeds, up_time_minutes, locations, algo_ids = nice_hash_client.get_mining_rigs()
         connected_rig_names = set(rig_names)
 
-        for rig_name_to_monitor in rig_names_to_monitor:
+        for i, rig_name_to_monitor in enumerate(rig_names_to_monitor):
             if rig_name_to_monitor not in connected_rig_names:
-                email_sender.send_email(email_subject='Host not connected',
-                                        email_content='[{}] appears not to be connected. Please check.'.format(
-                                            rig_name_to_monitor))
+                rig_statuses[i] = False
+
+                # we notify when there is a change in the status. Not every time we pull from the API.
+                if previous_rig_statuses[i] != rig_statuses[i]:
+                    email_sender.send_email(email_subject='Host not connected',
+                                            email_content='[{}] appears not to be connected. Please check.'.format(
+                                                rig_name_to_monitor))
             else:
                 logger.debug('{} is connected.'.format(rig_name_to_monitor))
+                rig_statuses[i] = True
 
+        previous_rig_statuses = list(rig_statuses)
         logger.debug('Going to sleep for {} seconds.'.format(polling_interval_sec))
         sleep(polling_interval_sec)
 
