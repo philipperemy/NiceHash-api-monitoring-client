9c2eb6858ccba4f8176e46a1ea6ca5fc9fc38f1f
philipperemy <premy@reactive.co.jp>
2017-06-17 18:37:41 +0900

    same subject for all the emails

--- monitor.py
+++ monitor.py
@@ -6,24 +6,26 @@ from utils import send_email_notification, get_btc_usd_rate
 
 
 class AlertEmailSender:
-    def __init__(self, gmail_user, gmail_password, target_email):
+    def __init__(self, gmail_user, gmail_password, target_email, email_subject):
         self.gmail_user = gmail_user
         self.gmail_password = gmail_password
         self.target_email = target_email
+        self.email_subject = email_subject
 
-    def send_email(self, email_subject, email_content):
+    def send_email(self, email_content):
         send_email_notification(self.gmail_user,
                                 self.gmail_password,
                                 self.target_email,
                                 email_content,
-                                email_subject)
-        logger.debug('Email sent for subject = {}, content = {}'.format(email_subject, email_content))
+                                self.email_subject)
+        logger.debug('Email sent for subject = {}, content = {}'.format(self.email_subject, email_content))
 
 
 def run_monitoring_tool():
     email_sender = AlertEmailSender(c.MAIL.GMAIL_USERNAME,
                                     c.MAIL.GMAIL_PASSWORD,
-                                    c.MAIL.NOTIFICATION_EMAIL)
+                                    c.MAIL.NOTIFICATION_EMAIL,
+                                    c.MAIL.EMAIL_SUBJECT)
 
     # part online - offline
     addr = c.BITCOIN_WALLET_PUBLIC_ID
@@ -50,16 +52,14 @@ def run_monitoring_tool():
                 logger.debug('{} is down.'.format(rig_name_to_monitor))
                 rig_statuses[i] = False
                 if previous_rig_statuses[i] is True:
-                    email_sender.send_email(email_subject='Host not connected',
-                                            email_content='[{}] is down. Please check.'.format(
-                                                rig_name_to_monitor))
+                    email_sender.send_email(email_content='[{}] host is down. Please check.'.format(
+                        rig_name_to_monitor))
             else:
                 logger.debug('{} is connected.'.format(rig_name_to_monitor))
                 rig_statuses[i] = True
                 if previous_rig_statuses[i] is False:
-                    email_sender.send_email(email_subject='Host successfully connected',
-                                            email_content='[{}] appears to be connected. Congrats.'.format(
-                                                rig_name_to_monitor))
+                    email_sender.send_email(email_content='[{}] host successfully connected.'.format(
+                        rig_name_to_monitor))
 
         previous_rig_statuses = list(rig_statuses)
 
@@ -69,9 +69,8 @@ def run_monitoring_tool():
         unpaid_balance_fiat = unpaid_balance_btc * price_for_one_btc_in_fiat_currency
 
         if (time() - last_balance_reporting_time) > interval_between_balance_reporting_sec:
-            email_sender.send_email(email_subject='Unpaid Balance',
-                                    email_content='Your unpaid balance is now {0:.8f} BTC ({1:.2f} {2} approx).'.format(
-                                        unpaid_balance_btc, unpaid_balance_fiat, ref_fiat_currency))
+            email_sender.send_email(email_content='Your unpaid balance is now {0:.8f} BTC ({1:.2f} {2} approx).'.format(
+                unpaid_balance_btc, unpaid_balance_fiat, ref_fiat_currency))
             last_balance_reporting_time = time()
 
         logger.debug('Going to sleep for {} seconds.'.format(polling_interval_sec))
